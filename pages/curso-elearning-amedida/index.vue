95bb5685eb1bff074ffcfef710122febbfd41252
edson-edudigital <nhatrabalho22@gmail.com>
2022-08-18 12:02:19 -0100

    Update Curso eLearning
    Inserção de novas seções à página de curso eLearning.

--- pages/curso-elearning-amedida/index.vue
+++ pages/curso-elearning-amedida/index.vue
@@ -13,7 +13,46 @@
                 <img width="400" ref="cartoon" src="https://edudigital.pt/home/images/Grupo_821.png" alt="" srcset="">
             </template>
         </banner>
-    
+
+        <sectionduble
+          :id="'producao-eLearning-personalizado'"
+          :background="'var(--color-verde)'"
+          :img="'https://edudigital.pt/home/images/PCcomCurso-04-11.png'"
+          :flexDirection="'row'"
+          :color="'#fff'"
+          >
+          <template #text>
+            <Title :title="'Produção eLearning personalizado'" />
+            <p>Os nossos learning designers são especialistas em transformar os seus 
+              manuais existentes em formação interativa de qualidade. Se tem formação presencial já pronta, 
+              nós redesenhamos para experiências de eLearning envolventes e multi-dispositivo. Personalizado com o seu branding, 
+              cada curso à medida pode incluir as últimas inovações, desde interação social até adaptativo.</p>
+            <div>
+              <NuxtLink class="btn btn-primary" to="#">Saiba Mais</NuxtLink>
+            </div>
+
+          </template>
+
+        </sectionduble>
+
+        <sectionduble
+          :id="'motion-graphics-gamification'"
+          :background="'#d35640'"
+          :img="'https://edudigital.pt/home/images/Grupo_819.png'"
+          :flexDirection="'row-reverse'"
+          :color="'#fff'"
+          >
+          <template #text>
+            <Title :title="'Motion Graphics & Gamification'" />
+            <p>Para um nível acima de soluções de eLearning, realizamos gravações vídeo e locuções, 
+              animações em motion graphics ou realçamos formatos de eLearning tradicionais com mecânicas de jogo ou aprendizagem social, para uma aprendizagem de futuro.</p>
+            <div>
+              <NuxtLink class="btn btn-secondary" to="#">Saiba Mais</NuxtLink>
+            </div>
+          </template>
+
+        </sectionduble>
+        
         
         <contactform />
    
@@ -27,9 +66,11 @@
 <script>
 import { upDownAnimation } from '~/assets/js/animate'
 import banner from '~/components/banner.vue'
+import Title from '~/components/elements/title.vue'
 import Contactform from '~/components/contactform.vue';
+import Sectionduble from '~/components/sections/sectiondubleCartoon.vue';
 export default {
-  components: { banner, Contactform },
+  components: { banner, Title, Sectionduble, Contactform },
   methods:{
     animeCartoon(){
       upDownAnimation(this.$refs.cartoon,true);
