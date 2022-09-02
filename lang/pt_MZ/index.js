bbcc8f615b9130fe2dfb2d383a5813ecfaa905e7
edson-edudigital <nhatrabalho22@gmail.com>
2022-09-02 10:16:25 -0100

    sobre nos_MZ

--- lang/pt_MZ/index.js
+++ lang/pt_MZ/index.js
@@ -3,6 +3,7 @@ import _curso_elearning_amedida from "./_curso_elearning_amedida";
 import _elearning_plataforms from "./_elearning_platforms";
 import _home from "./_home";
 import _menus from "./_menus";
+import _sobre_nos from "./_sobre_nos";
 
 export default {
 
@@ -10,6 +11,7 @@ export default {
     menus: _menus,
     contactform: _contactform,
     elearning_platform: _elearning_plataforms,
-    curso_elearning_amedida: _curso_elearning_amedida
+    curso_elearning_amedida: _curso_elearning_amedida,
+    sobre_nos: _sobre_nos
 
 }
